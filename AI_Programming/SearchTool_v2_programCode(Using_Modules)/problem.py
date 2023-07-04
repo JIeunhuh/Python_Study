33f8f73a391ecea4dc7d1b561130413a93b20ee8
JIeunhuh <entks301@naver.com>
2023-07-04 18:00:55 +0900

    Class만들기(상속)

---
+++ AI_Programming/SearchTool_v2_programCode(Using_Modules)/problem.py
@@ -0,0 +1,14 @@
+import math
+import random as rd
+
+class Problem:
+    def __init__(self):
+        pass
+    
+class Numeric(Problem):
+    def __init__(self):
+        super().__init__(self)
+    
+class Tsp(Problem):
+    def __init__(self):
+        super().__init__(self)
