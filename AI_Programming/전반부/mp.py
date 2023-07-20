a13ff5b33f1a9ff617c8643b00f5441025174a69
JIeunhuh <entks301@naver.com>
2023-07-20 14:38:05 +0900

    230720 AI Programming(전반부)

---
+++ AI_Programming/전반부/mp.py
@@ -0,0 +1,13 @@
+import time
+from multiprocessing import Pool
+
+def work(x):
+    print(x)
+    
+if __name__ == '__main__':
+    pool = Pool(4)
+    data = range(1,100)
+    t0 = time.time_ns()
+    pool.map(work, data)
+    t1 = time.time_ns()
+    print(f'Time:{t1}-{t0}')
